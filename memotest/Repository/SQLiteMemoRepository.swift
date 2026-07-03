d01cb45dff6d2db1fa81ea7c6cb2486365ab2761
shun-NMR93 <nomurashiyun@gmail.com>
2026-07-04 00:18:06 +0900

    load機能の実装

--- memotest/Repository/SQLiteMemoRepository.swift
+++ memotest/Repository/SQLiteMemoRepository.swift
@@ -28,10 +28,45 @@ class SQLiteMemoRepository: MemoRepositoryProtocol {
             memo.updatedAt.timeIntervalSince1970
         ]
         
-        DatabaseManager.execute(sql, bindings: bindings)
+        DatabaseManager.shared.executeSQL(sql, bindings: bindings)
         
     }
     func load(id: UUID) -> Memo? {
+        
+        let sql = """
+        SELECT * FROM memo WHERE id = ?
+        """
+        
+        var result: Memo?
+        
+        var statement: OpaquePointer?
+        
+        guard sqlite3_prepare_v2(DatabaseManager.shared.db, sql, -1, &statement, nil) == SQLITE_OK else {
+            print("prepare error")
+            return nil
+        }
+        
+        sqlite3_bind_text(statement, 1, id.uuidString, -1, nil)
+
+        if sqlite3_step(statement) == SQLITE_ROW {
+            
+            let idString = String(cString: sqlite3_column_text(statement, 0))
+            let title = String(cString: sqlite3_column_text(statement, 1))
+            let body = String(cString: sqlite3_column_text(statement, 2))
+            
+            let createdAt = Date(timeIntervalSince1970: sqlite3_column_double(statement, 3))
+            let updatedAt = Date(timeIntervalSince1970: sqlite3_column_double(statement, 4))
+            
+            result = Memo(
+                id: UUID(uuidString: idString)!,
+                title: title,
+                body: body,
+                createdAt: createdAt,
+                updatedAt: updatedAt
+            )
+            
+        }
+        
         
     }
     func loadAll() -> [Memo] {
